c19fd699e31073ea0f01be26ec010290998804ab
Samuel J Wilkins <wilkins.samuelj@gmail.com>
2019-12-02 09:03:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,9 +25,20 @@ function viewCart() {
   }
   else if (cart.length === 2) {
     var tempString = `In your cart, you have `
-    for (var i = 0; i < cart.length; i++) {
-      var groceryItem = Object.values(cart[i])
-      tempString = concat(tempString + `${groceryItem[0]} at $${groceryItem[1]}.`)
+    var groceryItem = Object.values(cart[0])
+    tempString = concat(tempString + `${groceryItem[0]} at $${groceryItem[1]},`)
+    var groceryItem2 = Object.values(cart[1])
+    tempString = concat(tempString + ` and ${groceryItem2[0]} at $${groceryItem2[1]}.`)
+  }
+  else {
+    else if (cart.length >= 3) {
+      var tempString = `In your cart, you have `
+      var groceryItem = Object.values(cart[0])
+      tempString = concat(tempString + `${groceryItem[0]} at $${groceryItem[1]},`)
+      var groceryItem2 = Object.values(cart[1])
+      tempString = concat(tempString + ` ${groceryItem2[0]} at $${groceryItem2[1]},`)
+      var groceryItem3 = Object.values(cart[2])
+      tempString = concat(tempString + ` and ${groceryItem3[0]} at $${groceryItem3[1]}.`)
     }
   }
 }
